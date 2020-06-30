8de6a439db5691ab2c3ba7f2e1c3a273eca8e1f3
apple01644 <banana2692@daum.net>
2020-06-30 18:09:44 +0900

    Change Schedule

--- res/schedule.py
+++ res/schedule.py
@@ -3,9 +3,9 @@ import datetime
 schedule = {
     '평일': {
         datetime.time(hour=6, minute=17): '기상송',
-        datetime.time(hour=7, minute=8): '3학년아침',
-        datetime.time(hour=7, minute=13): '2학년아침',
-        datetime.time(hour=7, minute=18): '1학년아침',
+        datetime.time(hour=7, minute=13): '3학년아침',
+        datetime.time(hour=7, minute=18): '2학년아침',
+        datetime.time(hour=7, minute=23): '1학년아침',
         datetime.time(hour=7, minute=57): '기상송',
         datetime.time(hour=21, minute=17): '입구폐쇄',
         datetime.time(hour=22, minute=28): '점호준비',
